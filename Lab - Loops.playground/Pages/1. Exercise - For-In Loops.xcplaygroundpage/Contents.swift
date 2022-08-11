916b9cbc9a10b0872530114dd342b966e824fcfd
mac book <roozh1009@gmail.com>
2022-08-11 06:17:20 +0300

    roz

---
+++ Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+## Exercise - For-In Loops
+ 
+ Create a for-in loop that loops through values 1 to 100, and prints each of the values.
+ */
+
+import Foundation
+for R in 1...100 {
+    print(R)
+}
+
+//:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for A in alphabet
+{
+    print(A)
+}
+//:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
+var dich:[String:String] = ["Available":"A", "Busy":"B","Closed": "C"]
+for i in dich {
+    print("\(i.key) is \(i.value)")
+}
+
+/*:
+page 1 of 6  |  [Next: App Exercise - Movements](@next)
+ */
